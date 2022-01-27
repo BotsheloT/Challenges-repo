0de89f7f055902b5ecaaabd334bd1cd4e6c099fb
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-01-27 13:19:21 +0200

    Begining of functional code addition

---
+++ Age_Calc/sandbox.js
@@ -0,0 +1,20 @@
+// Function to calculate a user's age
+let calc = (year, month) =>{
+
+}
+
+//Variable for button selection using DOM
+let btn = document.querySelector('.btn');
+
+//Button click code
+btn.onclick = function(){
+    //Querying all input for final output
+
+    //Name input
+    let name = document.querySelector('.name').value;
+    let surname = document.querySelector('.sur').value;
+
+    //Date of birth input
+    let dob = document.querySelector('.dob');
+    let bYear = dob.year();
+}
