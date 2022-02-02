baad830d26484f21f1bfc693cae5823ec51a8246
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-02-02 11:02:41 +0200

    Age calculation feature addition

--- Age_Calc/sandbox.js
+++ Age_Calc/sandbox.js
@@ -1,12 +1,20 @@
-//Variables for current date
-let nowDate = new Date;
-let nyear = nowDate.year;
-let nmonth = nowDate.month;
-let nday = nowDate;
-
 // Function to calculate a user's age
-let calc = (year, month) =>{
+let calc = (bDate) =>{
+    //Variables for current date
+    let nowDate = new Date;
+    let nYear = nowDate.year;
+    let nMonth = nowDate.month;
+    let nDay = nowDate.getDay;
 
+    //Age calculation
+    if(nMonth < bDate.month){
+        return nYear - (bDate.year -1);
+    } else if (nMonth > bDate.month){
+        return nYear - bDate.year;
+    } else if ((nMonth == month) && (nDay <= bDate.getDay)){
+        return nYear - bDate.year;
+    } else return nYear - (bDate.year -1);
+            
 }
 
 //Variable for button selection using DOM
@@ -21,6 +29,11 @@ btn.onclick = function(){
     let surname = document.querySelector('.sur').value;
 
     //Date of birth input
-    let dob = document.querySelector('.dob');
-    let bYear = dob.year();
+    let dob = document.querySelector('.dob').value;    
+    
+    //Age Calculation
+    let age = calc(dob);
+
+    // Final output
+    alert(`${name} ${surname} is ${age} years old right now`);
 }
