8148b8a0a5963847abf04293443629c9bcc219d9
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-02-03 11:00:04 +0200

    Area calculation functions added

---
+++ Area_Calc/sandbox.js
@@ -0,0 +1,10 @@
+//Cirlce Area function
+const calcArea = radius => 3.14 * radius**2;
+
+//Square Area function
+const squareArea = side => side**side;
+
+//Traingle Area function
+const triArea = (base, height) => {
+    return 0.5*base*height;
+}
